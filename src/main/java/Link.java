eb5b5b7eb97edfa24b4f9c8a029242d8134ea4e1
Andrey Zhozhin <andrey.zhozhin@gmail.com>
2012-10-18 17:38:42 +0600

    added some utility classes

---
+++ src/main/java/Link.java
@@ -0,0 +1,19 @@
+
+public class Link {
+
+	private String title;
+	private String href;
+	
+	public Link(String title, String href) {
+		this.title=title;
+		this.href=href;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public String getHref() {
+		return href;
+	}
+
+	
+}
