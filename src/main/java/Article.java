517ec1ff297ffca3699bd5ebbdfff909f8c8ebee
Andrey Zhozhin <andrey.zhozhin@gmail.com>
2012-10-18 17:40:57 +0600

    removed hashCode

--- src/main/java/Article.java
+++ src/main/java/Article.java
@@ -90,19 +90,6 @@ public class Article {
 	}
 
 	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result
-				+ ((fullName == null) ? 0 : fullName.hashCode());
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result
-				+ ((shortName == null) ? 0 : shortName.hashCode());
-		result = prime * result + ((text == null) ? 0 : text.hashCode());
-		return result;
-	}
-
-	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
--- src/main/java/Section.java
+++ src/main/java/Section.java
@@ -142,19 +142,6 @@ public class Section {
 		}
 	}
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result
-				+ ((fullName == null) ? 0 : fullName.hashCode());
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((parent == null) ? 0 : parent.hashCode());
-		result = prime * result + (root ? 1231 : 1237);
-		result = prime * result
-				+ ((shortName == null) ? 0 : shortName.hashCode());
-		return result;
-	}
 
 	@Override
 	public boolean equals(Object obj) {
